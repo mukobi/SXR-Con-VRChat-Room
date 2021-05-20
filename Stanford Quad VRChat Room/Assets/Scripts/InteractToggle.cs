db72d263db648bf638532217c4ab19107447a294
Gabriel Mukobi <gabrielmukobi@gmail.com>
2021-05-20 01:14:46 -0700

    Add mirror toggling

---
+++ Stanford Quad VRChat Room/Assets/Scripts/InteractToggle.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class InteractToggle : UdonSharpBehaviour
+{
+    public GameObject target;
+
+    public override void Interact()
+    {
+        target.SetActive(!target.activeSelf);
+    }
+}
