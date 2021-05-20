0a13b6fecf8419ec7721f47100f720e6ed62cb45
Gabriel Mukobi <gabrielmukobi@gmail.com>
2021-05-20 04:40:06 -0700

    Make things funner

--- Stanford Quad VRChat Room/Assets/Scripts/RespawnAfterDuration.cs
+++ Stanford Quad VRChat Room/Assets/Scripts/RespawnAfterDuration.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
@@ -6,8 +7,45 @@ using VRC.Udon;
 
 public class RespawnAfterDuration : UdonSharpBehaviour
 {
+    public float Duration = 10.0f;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private bool canRespawn = false;
+    private float timeOfLastDrop = 0.0f;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnPickup()
+    {
+        canRespawn = false;
+    }
+
+    public override void OnDrop()
+    {
+        timeOfLastDrop = Time.time;
+        canRespawn = true;
+    }
+
+    private void Update()
+    {
+        if (canRespawn && Time.time - timeOfLastDrop > Duration)
+        {
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        canRespawn = false;
+        transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
     }
 }
