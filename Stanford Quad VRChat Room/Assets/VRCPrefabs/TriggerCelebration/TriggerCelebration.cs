061f0ecd6364fd0b28878a8f5fbb1123e3819b1f
Gabriel Mukobi <gabrielmukobi@gmail.com>
2021-05-22 02:49:55 -0700

    Done fixing things

---
+++ Stanford Quad VRChat Room/Assets/VRCPrefabs/TriggerCelebration/TriggerCelebration.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+/// <summary>
+/// Plays a celebration particle and audio effect when a player enters the trigger area.
+/// </summary>
+public class TriggerCelebration : UdonSharpBehaviour
+{
+    private AudioSource audioSource;
+    private ParticleSystem particles;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Celebrate");
+    }
+
+    public void Celebrate()
+    {
+        if (audioSource)
+            audioSource.Play();
+        if (particles)
+            particles.Play();
+    }
+}
